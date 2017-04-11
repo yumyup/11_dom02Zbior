380e27a71dbce47ef0cef6a0bdd73cf6082bcb9a
yupi <grafik@10g.pl>
2017-04-11 14:49:30 +0200

    pomysł na rozwiazanie ale program nie działa

--- src/Set.java
+++ src/Set.java
@@ -1,15 +1,23 @@
 import java.util.Random;
 import java.util.TreeSet;
 
-/**
- * Created by Helmut on 2017-04-11.
- */
+
 public class Set {
     public static void main(String[] args) {
         TreeSet<Integer> set = new TreeSet<>();
-        for (int i = 0; i <51; i++){
-        set.add(Random.nextInt(1000));
-        int first = set.first();
+        int sum = 0;
+        for (int i = 0; i <= 50; i++) {
+            set.add(Random.nextInt(1000));
+            sum = sum + i;
+        }
+        double srednia = sum / 50;
+        double bigger = srednia < i;
+
+
+        System.out.println(set.last());
+        System.out.println(set.first());
+        System.out.println(srednia);
+        System.out.println(bigger);
 
     }
 }
